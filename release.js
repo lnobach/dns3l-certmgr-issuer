57d91a187119a91e162a4d253ce412415d214358
Leonhard Nobach <leonhard.nobach@telekom.de>
2026-08-24 18:18:57 +0200

    use commonjs instead of es

--- release.js
+++ release.js
@@ -1,10 +1,11 @@
 // npx semantic-release -e ./release.js
 
-export const dryRun = false;
-export const plugins = [
-  "@semantic-release/release-notes-generator",
-  ["@semantic-release/npm", {
-    npmPublish: false
-  }],
-  ["@semantic-release/github", { addReleases: "top" }]
-];
+module.exports = {
+  dryRun: false,
+  plugins: [
+    "@semantic-release/release-notes-generator",
+    [ "@semantic-release/npm", {
+        npmPublish: false } ],
+    [ "@semantic-release/github", { addReleases: "top" } ]
+  ]
+};
